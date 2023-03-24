15945e4d45ec51f1747f657cd87e019ef41ffb41
tormelo <23294699+tormelo@users.noreply.github.com>
2023-03-24 18:25:19 -0300

    test: add tests to the sort_by function of sorting.py module

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,16 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = read("data/jobs.csv")
+    assert jobs[0]["id"] == "0"
+
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["id"] == "2191"
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["id"] == "2105"
+
+    sort_by(jobs, "date_posted")
+    assert jobs[0]["id"] == "7"
