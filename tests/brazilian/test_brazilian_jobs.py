028f9b248bd5b3d2ec43aa361bd7af3add1b2462
tormelo <23294699+tormelo@users.noreply.github.com>
2023-03-24 18:00:07 -0300

    test: add tests to the read_brazilian_file function of brazilian_jobs.py module

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    translated_list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    expected_keys = ["title", "salary", "type"]
+
+    for job in translated_list:
+        for key in expected_keys:
+            assert key in job
