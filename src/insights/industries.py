b848ce02c75ca8586765530b55450475e439af50
tormelo <23294699+tormelo@users.noreply.github.com>
2023-03-24 15:47:42 -0300

    feat: add get_unique_industries function to industries.py module

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,13 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    jobs = read(path)
+    industries_set = {job["industry"] for job in jobs}
+    industries_set.discard("")
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return list(industries_set)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
